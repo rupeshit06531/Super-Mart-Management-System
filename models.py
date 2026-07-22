012fd3865c1280c1b7710dfd3d3f23728d25793d
Rupesh Kumar Mahto <rupeshit06531@gmail.com>
2026-07-22 15:23:19 +0530

    Added Product database model

--- models.py
+++ models.py
@@ -5,3 +5,10 @@ class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
